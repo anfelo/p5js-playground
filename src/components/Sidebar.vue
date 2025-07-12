0abc2604815b68d2643237d735deadb0b229e1a1
Andres Osorio <af.osorio1341@gmail.com>
2025-07-12 16:19:06 +0200

    create multiple level menu

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,14 +3,32 @@ import Menu from 'primevue/menu'
 import type { MenuItem } from 'primevue/menuitem'
 
 const items: MenuItem[] = [
-  { label: 'Walker', url: '/walker' },
-  { label: 'Body', url: '/body' },
-  { label: 'Liquid', url: '/liquid' },
-  { label: 'Oscillator', url: '/oscillator' },
-  { label: 'Wave', url: '/wave' },
-  { label: 'Spring', url: '/spring' },
-  { label: 'Pendulum', url: '/pendulum' },
-  { label: 'Butterfly', url: '/butterfly' },
+  {
+    label: 'Randomness',
+    items: [
+      {
+        label: 'Walker',
+        url: '/walker',
+      },
+    ],
+  },
+  {
+    label: 'Forces',
+    items: [
+      { label: 'Body', url: '/body' },
+      { label: 'Liquid', url: '/liquid' },
+    ],
+  },
+  {
+    label: 'Oscillation',
+    items: [
+      { label: 'Oscillator', url: '/oscillator' },
+      { label: 'Wave', url: '/wave' },
+      { label: 'Spring', url: '/spring' },
+      { label: 'Pendulum', url: '/pendulum' },
+      { label: 'Butterfly', url: '/butterfly' },
+    ],
+  },
 ]
 </script>
 
